46572d81221bb1c239845d8ee888aae8e6ff6fc0
Khushi Singh <khushi@example.com>
2025-06-09 04:13:42 +0530

    new commit today

---
+++ trycatch.js
@@ -0,0 +1,30 @@
+setTimeout(() => {
+    console.log("Hacking wifi....Please wait...");
+}, 1000);
+
+try{
+    setTimeout(() => {
+        try{
+            console.log(rahul);
+        } catch(error){
+
+        }    
+    }, 100);
+   
+} catch(error){
+    console.log("Balle Balle"); 
+}
+
+
+setTimeout(() => {
+    console.log("Fetching username and password ....Please wait...");
+}, 2000);
+
+setTimeout(() => {
+    console.log("Hacking Rahul's facebook id.....Please wait...");
+}, 3000);
+
+setTimeout(() => {
+    console.log("Username and password of Rahul (+918648415154) fetched....Please wait....");
+}, 4000);
+
