e08abe1a904157823a08594869e46469d16f18a3
Khushi Singh <khushi@example.com>
2025-06-09 12:06:13 +0530

    another new commit

--- trycatch.js
+++ trycatch.js
@@ -1,30 +1,46 @@
-setTimeout(() => {
-    console.log("Hacking wifi....Please wait...");
-}, 1000);
+// setTimeout(() => {
+//     console.log("Hacking wifi....Please wait...");
+// }, 1000);
+
+// try{
+//     setTimeout(() => {
+//         try{
+//             console.log(rahul);
+//         } catch(error){
+
+//         }    
+//     }, 100);
+   
+// } catch(error){
+//     console.log("Balle Balle"); 
+// }
+
+
+// setTimeout(() => {
+//     console.log("Fetching username and password ....Please wait...");
+// }, 2000);
+
+// setTimeout(() => {
+//     console.log("Hacking Rahul's facebook id.....Please wait...");
+// }, 3000);
+
+// setTimeout(() => {
+//     console.log("Username and password of Rahul (+918648415154) fetched....Please wait....");
+// }, 4000);
+
+
+//finally
 
 try{
-    setTimeout(() => {
-        try{
-            console.log(rahul);
-        } catch(error){
-
-        }    
-    }, 100);
-   
-} catch(error){
-    console.log("Balle Balle"); 
+    let a =0;
+    console.log(program);
+    console.log("Program ran successfully");
 }
-
-
-setTimeout(() => {
-    console.log("Fetching username and password ....Please wait...");
-}, 2000);
-
-setTimeout(() => {
-    console.log("Hacking Rahul's facebook id.....Please wait...");
-}, 3000);
-
-setTimeout(() => {
-    console.log("Username and password of Rahul (+918648415154) fetched....Please wait....");
-}, 4000);
-
+catch(err){
+    console.log("This is an error");
+    console.log(p);
+}
+ 
+finally{
+    console.log("I am a good boy");
+}
